287c91df305fedd782f0b67de1e5fc9e09ff6247
thyebyi <46091485+thyebyi@users.noreply.github.com>
2020-01-26 22:57:16 +0300

    Add first prac

---
+++ ServerDevelop.LabWorks/UdpFileTransfer/Client.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpFileTransfer
+{
+    class Client : UdpClient
+    {
+        public void SendFile(string path, string ip, int port = 8000)
+        {
+            if (File.Exists(path))
+            {
+                FileInfo info = new FileInfo(path);
+                byte[] data = File.ReadAllBytes(path);
+
+                UdpClient sender = new UdpClient();
+                try
+                {
+                    sender.Send(data, (int)info.Length, ip, port);
+                    Console.WriteLine("File sended.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    sender.Close();
+                }
+            }
+            else throw new FileNotFoundException();
+        }
+
+        public void ReceiveFile(string ip, int port = 8000)
+        {
+            UdpClient receiver = new UdpClient(port);
+            IPEndPoint remoteIp = new IPEndPoint(IPAddress.Parse(ip), port);
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting...");
+                    byte[] data = receiver.Receive(ref remoteIp);
+                    Console.Write("Enter saved file name: ");
+                    string fileName = Console.ReadLine();
+                    if (fileName == "") throw new ArgumentException();
+
+                    string decodedData = Encoding.Default.GetString(data);
+                    var stream = new StreamWriter(fileName, false, Encoding.Default);
+                    stream.Write(decodedData);
+                    stream.Close();
+                    Console.WriteLine("File recieved.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                receiver.Close();
+            }
+        }
+    }
+}
---
+++ ServerDevelop.LabWorks/UdpFileTransfer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UdpFileTransfer
+{
+    class Program
+    {
+        static string remoteAddress;
+        static int remotePort;
+        static int localPort;
+        static string Filepath;
+        static Client Client;
+
+        static void Main(string[] args)
+        {
+            Client = new Client();
+            try
+            {
+                Console.Write("Enter the ip adress to connect: ");
+                remoteAddress = Console.ReadLine();
+                Console.WriteLine("Choose an action \n 1. Send file \n 2. Recieve file \n");
+                int choice = int.Parse(Console.ReadKey().ToString());
+                if (choice == 1)
+                {
+                    Console.Write("Enter the port to connect: ");
+                    remotePort = int.Parse(Console.ReadLine());
+                    Console.Write("Enter file path: ");
+                    Filepath = Console.ReadLine();
+                    ConfirmAndSendFile();
+                }
+                else if (choice == 2)
+                {
+                    Console.Write("Enter the port to recieve: ");
+                    localPort = int.Parse(Console.ReadLine());
+                    Thread receiveThread = new Thread(new ThreadStart(StartReceiving));
+                    receiveThread.Start();
+                }
+                else throw new ArgumentException();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.Message);
+            }
+            Console.ReadKey();
+        }
+
+        static void StartReceiving()
+        {
+            Client.ReceiveFile(remoteAddress, localPort);
+        }
+
+        static void ConfirmAndSendFile()
+        {
+            Console.WriteLine($"Send file {Filepath}? y/n: ");
+
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                Console.WriteLine();
+                Client.SendFile(Filepath, remoteAddress, remotePort);
+            }
+        }
+    }
+}
